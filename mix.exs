662a929aa6350e922a07530668340854a71c72d9
Carla Rodríguez Estévez <carla.rodriguez1@udc.es>
2023-03-22 23:31:56 +0100

    update ci test

--- mix.exs
+++ mix.exs
@@ -64,7 +64,9 @@ defmodule Protectora.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
+      "test.ci": ["ecto.drop", "ecto.create --quiet", "ecto.migrate", "test"]
+
     ]
   end
 end
