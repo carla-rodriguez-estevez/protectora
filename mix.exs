3620adbce51e7a412985725f2ce293c0dd413e2d
Carla Rodríguez Estévez <carla.rodriguez1@udc.es>
2023-03-26 01:46:13 +0100

    Added dialyxir

--- mix.exs
+++ mix.exs
@@ -48,7 +48,10 @@ defmodule Protectora.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      # You can make use of it by running mix dialyzer
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: true},
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
     ]
   end
 
